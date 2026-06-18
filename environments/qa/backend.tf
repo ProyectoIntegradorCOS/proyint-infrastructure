832b397ef50dbd5109749d8926ebab6a7293f2da
carlosormeno <carlos.ormeno@outlook.es>
2026-06-17 22:03:50 -0500

    fix(qa/prod): corregir staging→qa, formato HCL, defaults de región y backup_retention_period

--- environments/qa/backend.tf
+++ environments/qa/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "dmc-final-terrastate-app"
-    key            = "thaqhiri/staging/terraform.tfstate"
+    key            = "thaqhiri/qa/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "thaqhiri-terraform-locks"
     encrypt        = true
